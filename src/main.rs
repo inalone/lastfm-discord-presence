cff45d2e3077cdacb82305e113bad5b75049cf86
inalone <me@inal.one>
2022-06-10 17:05:02 +0100

    Good initial working state

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,63 @@
+use lastfm_rs::user::recent_tracks::Track;
+mod discord;
+use discord_sdk::activity::{ActivityBuilder, Assets};
+use std::time::SystemTime;
+
 const USER: &'static str = "mjghd";
 
-async fn check_now_playing() {
-    let mut client = lastfm_rs::Client::new("2c01e403bd06ab75896a9b431e138a6c");
-    let tracks = client.recent_tracks(USER).await.send().await;
+async fn update_discord_presence(discord_client: &discord::Client, track: &Track) {
+    let rp = ActivityBuilder::default()
+        .details(&track.name)
+        .state(&track.artist.name)
+        .assets(Assets {
+            large_image: Some(track.images[0].image_url.as_str().to_string()),
+            large_text: Some(track.album.name.as_str().to_string()),
+            small_image: Some(track.images[0].image_url.as_str().to_string()),
+            small_text: Some(track.album.name.as_str().to_string()),
+        })
+        .start_timestamp(SystemTime::now());
+
+    //            Assets::default()
+    // .large("the".to_owned(), Some("u mage".to_owned()))
+    // .small("the".to_owned(), Some("i mage".to_owned())),
+
+    println!(
+        "updated activity: {:?}",
+        discord_client.discord.update_activity(rp).await
+    );
+}
+
+async fn check_now_playing(discord_client: &discord::Client) {
+    let mut lastfm_client = lastfm_rs::Client::new("2c01e403bd06ab75896a9b431e138a6c");
+    let tracks = lastfm_client.recent_tracks(USER).await.send().await;
     if let Ok(tracks) = tracks {
         if tracks.tracks.is_empty() {
-            println!("No tracks for user {} found", USER);
             return;
         }
 
         let recent_track = &tracks.tracks[0];
         if let Some(attributes) = &recent_track.attrs {
             if attributes.now_playing != "true" {
-                println!("{} is not currently playing any track", USER);
                 return;
             }
 
-            println!("Now playing: {}", recent_track.name);
-        } else {
-            println!("{} is not currently playing any track", USER);
+            update_discord_presence(discord_client, recent_track).await;
         }
-    };
+    }
 }
 
 #[tokio::main]
 async fn main() {
-    check_now_playing().await;
+    let client = discord::make_client(discord_sdk::Subscriptions::ACTIVITY).await;
+
+    check_now_playing(&client).await;
+
+    let mut r = String::new();
+    let _ = std::io::stdin().read_line(&mut r);
+
+    println!(
+        "cleared activity: {:?}",
+        client.discord.clear_activity().await
+    );
+    client.discord.disconnect().await;
 }
